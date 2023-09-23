7b86f7ed3d3a6c33f15b74ed08a68dcfa4e52f39
krishna <devenger@192.168.1.11>
2023-09-23 20:39:15 +0530

    compilations issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "flowbite-react";
 import NavbarWithDropdown from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { RaiseAnOrder } from "./pages/RaiseAnOrder";
 import { Home } from "./pages/Home";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <div className="flex">
--- src/pages/RaiseAnOrder.tsx
+++ src/pages/RaiseAnOrder.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import {
   Button,
-  Checkbox,
   Label,
   TextInput,
   FileInput,
